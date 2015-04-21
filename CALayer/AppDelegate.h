3ad02920f7b84c4faca1cc242ed8e19df2b3db3a
sirko <admin@Mac-Admin.local>
2015-04-21 22:09:04 +0300

    Initial Commit

---
+++ CALayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CALayer
+//
+//  Created by sirko on 21/04/15.
+//  Copyright (c) 2015 sirko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
